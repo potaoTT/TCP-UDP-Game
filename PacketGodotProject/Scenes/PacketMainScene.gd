f4a326268d1dc5830c66c6b52c390e63a49ee238
Josh <72240549+potaoTT@users.noreply.github.com>
2023-11-17 10:21:22 +0800

    setting up more things

--- PacketGodotProject/Scenes/PacketMainScene.gd
+++ PacketGodotProject/Scenes/PacketMainScene.gd
@@ -1,16 +1,16 @@
 extends Node2D
 
+var currently_handling_packet = false
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func display_test():
+	pass
 
+func clear_text():
+	pass
+	
+	
+func dissamble_packet():
+	pass
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Checker_timeout():
+	pass
