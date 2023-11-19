7aab96ac45131cd37d42d260ba6accd999717810
Josh <72240549+potaoTT@users.noreply.github.com>
2023-11-19 15:11:56 +0800

    Packet Creator, Scan Packet, and Visual Things
    Total list of changes:
    1 . Rearranged the screen so it looks better and more terminal-ly
    2 . Added a function to create a packet
    3 . Added a function to scan a packet when the "Scan Packet" button is pressed
    4 . Added placeholder functions for Accept and Deny
    5 . Added placeholder variables for valid protocols, ips and keyword
    6 . Added a function to see if the person did the right thing, BUT it never used and is iffy. Need to be worked on

--- PacketGodotProject/Scenes/PacketMainScene.gd
+++ PacketGodotProject/Scenes/PacketMainScene.gd
@@ -1,16 +1,123 @@
 extends Node2D
 
-var currently_handling_packet = false
+enum CPI_state {SENDER_IP, DESTINATION_IP, PROTOCOL, WORD}
 
-func display_test():
-	pass
 
-func clear_text():
-	pass
+var rng = RandomNumberGenerator.new()
+
+var current_packet_info = []
+var CPI_word_array = ["Sender IP", "Destiantion IP", "Protocol", "Word"]
+#"Sender IP", "Destiantion IP", "Protocol", "Word" 
+#word is the keyword
+
+var valid_ip_range = "100.10"
+#100.10, 100, 100.10.1, for example
+var num_valid_octets = 1
+
+
+#-------------------------------------------------
+#Protocols will be a later want in thing
+#get ty to code related stuff to it
+
+var auto_invalid_protocols = []
+#list of invalid porotocal that must be denied
+
+var auto_valid_protocols = []
+#list of valid protocols that must be allowed
+#--------------------------------------------------
+
+var keyword = "ookook"
+#list of random words
+
+
+func _ready():
+	packet_creator()
+
+
+
+
+
+
+
+#come back to later
+func packet_creator():
+	current_packet_info = []
 	
 	
-func dissamble_packet():
-	pass
+	
+	while len(current_packet_info) != 2:
+		var valid_ip_rng = rng.randi_range(1, 100)
+		
+		var ip_starter = null
+		if valid_ip_rng <= 80:
+			ip_starter = valid_ip_range
+		else:
+			ip_starter = "101.10"
+			
+		var valid_end = "false"
+		
+		while valid_end == "false":
+			var third_octet = rng.randi_range(1,255)
+			var fourth_octet = rng.randi_range(1,255)
+			
+			var combined_octet = str(third_octet) + "." + str(fourth_octet)
+			
+			if combined_octet != valid_ip_range:
+				valid_end = combined_octet
+		
+		print(ip_starter)
+		print(valid_end)
+		var ip = ip_starter + "." + valid_end
+		
+		if len(current_packet_info) == 1:
+			if current_packet_info[0] != ip:
+				current_packet_info.append(ip)
+		else:
+			current_packet_info.append(ip)
+		
+	print(len(current_packet_info))
+	
+	current_packet_info.append("Protocol")
+	current_packet_info.append(keyword)
+	print(current_packet_info)
 
-func _on_Checker_timeout():
-	pass
+
+func check_if_correct_valid_packet():
+	#first check sender ip, the destination ip
+	var is_valid = true
+	if (valid_ip_range in current_packet_info[CPI_state.SENDER_IP]) == false:
+		is_valid = false
+	
+	if (valid_ip_range in current_packet_info[CPI_state.DESTINATION_IP]) == false:
+		is_valid = false
+	
+	return is_valid
+	
+	
+	
+func _on_Allow_pressed():
+	pass # Replace with function body.
+
+
+func _on_Deny_pressed():
+	pass # Replace with function body.
+
+
+func _on_Scan_pressed():
+	var packet_info_text_nodepath = "CanvasLayer/Background/H/Right/CR/CR2/Info/TBody/Text"
+	
+	var text_to_display = ""
+	for constant in CPI_state:
+		print(CPI_state)
+		print(constant)
+		var placement = CPI_state[constant]
+		var info = current_packet_info[placement]
+		var word = CPI_word_array[placement]
+		
+		text_to_display += "> " + word + " : " + info + " \n"	
+	
+	get_node(packet_info_text_nodepath).text = text_to_display
+
+
+
+
